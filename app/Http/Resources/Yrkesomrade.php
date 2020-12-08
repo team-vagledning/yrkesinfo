a85080eb88ed2ed415e1f6de146b4fbb33374ffe
Sebastian Kemi <sebastian.kemi@arbetsformedlingen.se>
2020-12-08 10:37:19 +0100

    Add ledigaJobb to response

--- app/Http/Resources/Yrkesomrade.php
+++ app/Http/Resources/Yrkesomrade.php
@@ -22,6 +22,7 @@ class Yrkesomrade extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'loner' => $this->aggregated_statistics['lon'],
+            'ledigaJobb' => $this->aggregated_statistics['ledigaJobb'],
             'anstallda' => $this->aggregated_statistics['anstallda'],
             'sektorer' => $this->aggregated_statistics['sektorer'],
             'regioner' => $this->aggregated_statistics['regioner'],
