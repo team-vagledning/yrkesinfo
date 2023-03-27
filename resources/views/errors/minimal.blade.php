23ae427ae193a99b46c7944d3d78b30c79c2d7b9
Sebastian Kemi <sebekish@gmail.com>
2023-03-27 15:05:50 +0200

    Clean up html error codes, no need to look fancy

---
+++ resources/views/errors/minimal.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title')</title>
+    </head>
+    <body>@yield('code') - @yield('message')</body>
+</html>
