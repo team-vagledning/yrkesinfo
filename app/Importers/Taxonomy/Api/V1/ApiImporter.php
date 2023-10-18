7034d19acaca356a629fb9f846cfdf144ae1ab6c
Sebastian Kemi <sebastian.kemi@arbetsformedlingen.se>
2023-10-18 11:13:41 +0200

    Revert "Merge regioner into one and the same, with both external_id as swedish standard and taxonomy_id as the id from the Jobtech taxonomy"
    This reverts commit cf429f02aa7f84769f3f7cac9107186736bc7704.

--- app/Importers/Taxonomy/Api/V1/ApiImporter.php
+++ app/Importers/Taxonomy/Api/V1/ApiImporter.php
@@ -134,8 +134,8 @@ class ApiImporter implements ImporterInterface
                 return true;
             }
 
-            Region::updateOrCreate(['name' => $region->{'taxonomy/preferred-label'}], [
-                'taxonomy_id' => $region->{'taxonomy/id'},
+            Region::updateOrCreate(['external_id' => $region->{'taxonomy/id'}], [
+                'external_id' => $region->{'taxonomy/id'},
                 'name' => $region->{'taxonomy/preferred-label'},
             ]);
         });
@@ -205,7 +205,7 @@ class ApiImporter implements ImporterInterface
 
             // Sync yrkesgrupp to yrkesområde, and also yrkesbenämningar
             $yrkesgrupp->yrkesomraden()->syncWithoutDetaching($yrkesomraden->pluck('id'));
-            $yrkesgrupp->yrkesbenamningar()->syncWithoutDetaching($yrkesbenamingar->pluck('id')->whereNotNull());
+            $yrkesgrupp->yrkesbenamningar()->syncWithoutDetaching($yrkesbenamingar->pluck('id'));
         });
 
         return $this;
